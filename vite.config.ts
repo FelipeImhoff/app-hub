078b61ef54ff36276f1b8771b6647e30f328e99e
FelipeImhoff <felipe_imhoff@hotmail.com>
2025-04-23 21:10:44 -0300

    allow umbrel.local as host

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: ['umbrel.local']
+    allowedHosts: ['umbrel.local'],
+    origin: 'http://umbrel.local:5173'
   }
 });
